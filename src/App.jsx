ef5772f203cc668c174a660ee91cf9d97976e011
enzo antilipi <enzoantilipi1@gmail.com>
2023-03-29 11:44:55 -0300

    actualizacion3

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,94 @@
 import "./App.css";
 import { db } from "./firebase/Config";
-import { addDoc, collection, doc, setDoc } from "firebase/firestore";
-import { Input } from "@nextui-org/react";
+import { addDoc, collection } from "firebase/firestore";
 import swal from "sweetalert";
+import { set, useForm } from "react-hook-form";
+import { useState } from "react";
+import { BsFillCalculatorFill } from "react-icons/bs";
 
 function App() {
-  const enviar = () => {
-    try {
-      const docRef = addDoc(collection(db, "usuarios"), {
-        nombre: document.getElementById("nombre").value,
-        apellido: document.getElementById("apellido").value,
-        correo: document.getElementById("correo").value,
-        DNI: document.getElementById("dni").value,
-        Tel: document.getElementById("tel").value,
-      });
-      console.log("Document written with ID: ", docRef.id);
-      swal({
-        title: "Buen trabajo",
-        text: "Sus datos se enviaron correctamente",
-        icon: "success",
-        button: "Aceptar",
-      });
-    } catch (e) {
-      console.error("Error adding document: ", e);
+  const enviar = (event) => {
+    event.preventDefault();
+    const nombre = document.getElementById("nombre").value;
+    const apellido = document.getElementById("apellido").value;
+    const correo = document.getElementById("correo").value;
+    const dni = document.getElementById("dni").value;
+    const tel = document.getElementById("tel").value;
+
+    if (nombre.length < 2) {
+      swal("El campo Nombre es obligatorio ");
+    } else if (apellido.length < 1) {
+      swal("El campo Apellido es obligatorio");
+    } else if (dni.length < 5) {
+      swal("El campo DNI es obligatorio");
+    } else if (tel.length < 5) {
+      swal("El campo Tel es obligatorio");
+    } else if (correo.length < 7) {
+      swal("El campo Correo es obligatorio");
+    } else {
+      try {
+        const docRef = addDoc(collection(db, "usuarios"), {
+          nombre: document.getElementById("nombre").value,
+          apellido: document.getElementById("apellido").value,
+          correo: document.getElementById("correo").value,
+          DNI: document.getElementById("dni").value,
+          Tel: document.getElementById("tel").value,
+        });
+
+        console.log("Document written with ID: ");
+        swal({
+          title: "Buen trabajo",
+          text: "Sus datos se enviaron correctamente",
+          icon: "success",
+          button: "Aceptar",
+        });
+      } catch {
+        console.error("Error adding document: ");
+      }
     }
   };
-  const saludo = () => {};
   return (
     <div>
-      {" "}
-      <div className="contenedor">
-        <h2>Solo para afiliados</h2>
-        <form>
-          <input type="nombre" placeholder="Nombre" id="nombre" />
-          <input type="apellido" placeholder="Apellido" id="apellido" />
-          <input type="text" placeholder="Correo" id="correo" />
-          {/* <input type="Password" placeholder="Password" /> */}
-          <input type="DNI" placeholder="DNI" id="dni" />
-          <input type="Tel" placeholder="Tel" id="tel" />
-        </form>
-        <button onClick={enviar}> Enviar</button>
+      <div className="contendorgloblal">
+        <div className="contenedor">
+          <h2>Ingreso a la calculadora de haberes</h2>
+          <form onSubmit={enviar}>
+            <input
+              id="nombre"
+              placeholder="Nombre"
+              name="nombre"
+              aria-describedby="emailHelp"
+            />
+            <input type="apellido" placeholder="Apellido" id="apellido" />
+
+            <input type="DNI" placeholder="DNI" id="dni" />
+
+            <input type="Tel" placeholder="Tel" id="tel" />
+
+            <input type="text" placeholder="Correo" id="correo" />
+
+            <button type="submit">Enviar</button>
+          </form>
+        </div>
+        <div className="contenedorEnl">
+          <a
+            href="https://main--elaborate-liger-087372.netlify.app/"
+            target="_blank"
+            className="enlace"
+          >
+            Calculadora de sueldo
+          </a>
+          <BsFillCalculatorFill className="icono" />
+        </div>
+      </div>
+      <div className="contenedorP">
+        <p>
+          La Calculadora de Haberes es para uso exclusivo de afiliadas/os y
+          permite corroborar si el sueldo se ha liquidado correctamente. Dado
+          que está en fase experimental, te pedimos que si encuentras algún
+          error nos lo hagas saber por correo electrónico a
+          secorganizacion@atech.org.ar
+        </p>
       </div>
     </div>
   );
